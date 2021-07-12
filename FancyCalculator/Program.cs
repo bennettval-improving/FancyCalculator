f6a485c919e5d8569dd92ebf80cb05a6f029bc62
Bennett Valentine <bennett.valentine@improving.com>
2021-07-12 08:34:57 -0500

    Can add two numbers and validate. (Steps 1-2) ➕

--- FancyCalculator/Program.cs
+++ FancyCalculator/Program.cs
@@ -6,7 +6,28 @@ namespace FancyCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("A Fancy Console Calculator");
+
+            Console.WriteLine("Enter a number");
+            var inputOne = Console.ReadLine();
+            decimal firstNumber;
+            if (!decimal.TryParse(inputOne, out firstNumber))
+            {
+                Console.WriteLine($"The first number, {inputOne}, was not a valid number.");
+                return;
+            }
+
+            Console.WriteLine("Enter a second number and I will add it to the first");
+            decimal secondNumber;
+            var inputTwo = Console.ReadLine();
+            if (!decimal.TryParse(inputTwo, out secondNumber))
+            {
+                Console.WriteLine($"The first number, {inputTwo}, was not a valid number.");
+                return;
+            }
+
+            var result = firstNumber + secondNumber;
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
