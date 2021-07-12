6587ad6dfd31730d76b8900f95d66681565c1e77
Bennett Valentine <bennett.valentine@improving.com>
2021-07-12 09:22:28 -0500

    Added multiple calculations per run of app. 👍

--- FancyCalculator/Program.cs
+++ FancyCalculator/Program.cs
@@ -7,27 +7,36 @@ namespace FancyCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("A Fancy Console Calculator");
+            string inputEquation = string.Empty;
 
             Console.WriteLine("Enter what you would like to see added or subtracted");
-            var inputEquation = Console.ReadLine();
-            var inputs = inputEquation.Split(' ');
+            inputEquation = Console.ReadLine();
 
-            decimal firstNumber;
-            if (!decimal.TryParse(inputs[0], out firstNumber))
+            while (!inputEquation.Equals("exit"))
             {
-                Console.WriteLine($"The first number, {inputs[0]}, was not a valid number.");
-                return;
-            }
+                var inputs = inputEquation.Split(' ');
 
-            decimal secondNumber;
-            if (!decimal.TryParse(inputs[2], out secondNumber))
-            {
-                Console.WriteLine($"The second number, {inputs[2]}, was not a valid number.");
-                return;
-            }
+                decimal firstNumber;
+                if (!decimal.TryParse(inputs[0], out firstNumber))
+                {
+                    Console.WriteLine($"The first number, {inputs[0]}, was not a valid number.");
+                    return;
+                }
 
-            var result = Calculate(firstNumber, secondNumber, inputs[1]);
-            if (result.HasValue) Console.WriteLine($"Result: {result}");
+                decimal secondNumber;
+                if (!decimal.TryParse(inputs[2], out secondNumber))
+                {
+                    Console.WriteLine($"The second number, {inputs[2]}, was not a valid number.");
+                    return;
+                }
+
+                var result = Calculate(firstNumber, secondNumber, inputs[1]);
+                if (result.HasValue) Console.WriteLine($"Result: {result}");
+                else return;
+
+                Console.WriteLine("Enter what you would like to see added or subtracted");
+                inputEquation = Console.ReadLine();
+            }
         }
 
         static decimal? Calculate(decimal firstNumber, decimal secondNumber, string opperation)
